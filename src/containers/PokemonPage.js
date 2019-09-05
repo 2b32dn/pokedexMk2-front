d1bb77df72a8c80d84dc33d963bf2ee189f21226
2b32dn <DicksonT_Lee@hotmail.com>
2019-09-05 00:35:45 -0400

    fsdfs

--- src/containers/PokemonPage.js
+++ src/containers/PokemonPage.js
@@ -1,26 +1,26 @@
-// import React, { Component } from 'react'
-// import {getPokemon} from '../API'
+import React, { Component } from 'react'
+import {getPokemon} from '../API'
 
-// class PokemonPage extends Component {
-//   state = {
-//     pokemon: null
-//   }
-//   componentDidMount = async () => {
-//     const { id } = this.props.match.params;
-//     const { pokemon } = await getPokemon(id);
-//     console.log(pokemon)
-//   }
-//   render() {
-//     console.log(this.props)
-//     const { pokemon } = this.state;
-//     return (
-//       (pokemon)
-//       ?
-//         <div>hi</div>
-//       :  
-//         <div>Loading</div>
-//     )
-//   }
-// }
+class PokemonPage extends Component {
+  state = {
+    pokemon: null
+  }
+  componentDidMount = async () => {
+    const { id } = this.props.match.params;
+    const { pokemon } = await getPokemon(id);
+    console.log(pokemon)
+  }
+  render() {
+    console.log(this.props)
+    const { pokemon } = this.state;
+    return (
+      (pokemon)
+      ?
+        <div>hi</div>
+      :  
+        <div>Loading</div>
+    )
+  }
+}
 
-// export default PokemonPage
+export default PokemonPage
