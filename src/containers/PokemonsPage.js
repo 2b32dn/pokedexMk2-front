4de6be7aee76c3fbfdd1f1bcd553758dffe2ea4f
2b32dn <DicksonT_Lee@hotmail.com>
2019-09-05 00:53:41 -0400

    shift alt f ppdsfdsf

--- src/containers/PokemonsPage.js
+++ src/containers/PokemonsPage.js
@@ -10,96 +10,96 @@ import Backdrop from '../components/modal/Backdrop';
 // import RightButton from '../components/buttons/RightButton';
 
 class PokemonsPage extends Component {
-	state = {
-		pokemons: null,
-		pokemon: null,
-		pokemonExtra: null,
-		pokemonTypes: null,
-		modalOpen: false
-	};
+  state = {
+    pokemons: null,
+    pokemon: null,
+    pokemonExtra: null,
+    pokemonTypes: null,
+      modalOpen: false
+  };
 
-	openModal = () => {
-		this.setState((e) => {
-			return {
-				modalOpen: !e.modalOpen
-			};
-		});
-	};
+  openModal = () => {
+    this.setState((e) => {
+      return {
+        modalOpen: !e.modalOpen
+      };
+    });
+  };
 
-	closeModal = () => {
-		this.setState({ modalOpen: false });
-	};
+  closeModal = () => {
+    this.setState({ modalOpen: false });
+  };
 
-	targetPokemon = async (id) => {
-		try {
-			const targetPokemonData = await fetchPokemonData(id);
-			this.setState({ pokemon: targetPokemonData });
-		} catch (err) {
-			console.log(err);
-		}
-	};
-	targetPokemonExtra = async (id) => {
-		try {
-			const targetPokemonData = await fetchPokemonExtraData(id);
-			this.setState({ pokemonExtra: targetPokemonData });
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  targetPokemon = async (id) => {
+    try {
+      const targetPokemonData = await fetchPokemonData(id);
+      this.setState({ pokemon: targetPokemonData });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  targetPokemonExtra = async (id) => {
+    try {
+      const targetPokemonData = await fetchPokemonExtraData(id);
+      this.setState({ pokemonExtra: targetPokemonData });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	componentDidMount = async () => {
-		try {
-			// Fetch pokemonsURL
-			const pokemons = await fetchPokemonsURL();
-			// console.log(pokemons)
-			const pokemonData = await Promise.all(
-				pokemons.results.map(async (result) => {
-					const pokemon = await fetch(result.url);
-					return pokemon.json();
-				})
-			);
-			this.setState({ pokemons: pokemonData });
+  componentDidMount = async () => {
+    try {
+      // Fetch pokemonsURL
+      const pokemons = await fetchPokemonsURL();
+      // console.log(pokemons)
+      const pokemonData = await Promise.all(
+        pokemons.results.map(async (result) => {
+          const pokemon = await fetch(result.url);
+          return pokemon.json();
+        })
+      );
+      this.setState({ pokemons: pokemonData });
 
-			const pokemonsTypes = await fetchPokemonTypes();
-			this.setState({ pokemonTypes: pokemonsTypes.results });
-		} catch (err) {
-			console.log(err);
-		}
-	};
+      const pokemonsTypes = await fetchPokemonTypes();
+      this.setState({ pokemonTypes: pokemonsTypes.results });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	render() {
-		const { pokemons, pokemon, pokemonExtra, pokemonTypes } = this.state;
-		// console.log('pokemons', pokemons)
-		// console.log("pokemon", pokemon)
-		// console.log("pokemonExtra", pokemonExtra)
-		// console.log(nextPokemons)
-		console.log(pokemonTypes);
+  render() {
+    const { pokemons, pokemon, pokemonExtra, pokemonTypes } = this.state;
+    // console.log('pokemons', pokemons)
+    // console.log("pokemon", pokemon)
+    // console.log("pokemonExtra", pokemonExtra)
+    // console.log(nextPokemons)
+    console.log(pokemonTypes);
 
-		let backDrop;
-		if (this.state.modalOpen) {
-			backDrop = <Backdrop closeModal={this.closeModal} />;
-		}
-		return pokemons ? (
-			<div className="pokemons-page">
-				<PokemonsList
-					pokemons={pokemons}
-					show={this.openModal}
-					targetPokemon={this.targetPokemon}
-					pokemonExtra={this.targetPokemonExtra}
-				/>
-				<Modal
-					pokemons={pokemons}
-					pokemon={pokemon}
-					pokemonExtra={pokemonExtra}
-					show={this.state.modalOpen}
-					pkmnTypes={this.state.pokemonTypes}
-				/>
-				<div>{backDrop}</div>
-			</div>
-		) : (
-				<div>Loading Pokemons...</div>
-			);
-	}
+    let backDrop;
+    if (this.state.modalOpen) {
+      backDrop = <Backdrop closeModal={this.closeModal} />;
+    }
+    return pokemons ? (
+      <div className="pokemons-page">
+        <PokemonsList
+          pokemons={pokemons}
+          show={this.openModal}
+          targetPokemon={this.targetPokemon}
+          pokemonExtra={this.targetPokemonExtra}
+        />
+        <Modal
+          pokemons={pokemons}
+          pokemon={pokemon}
+          pokemonExtra={pokemonExtra}
+          show={this.state.modalOpen}
+          pkmnTypes={this.state.pokemonTypes}
+        />
+        <div>{backDrop}</div>
+      </div>
+    ) : (
+        <div>Loading Pokemons...</div>
+      );
+  }
 }
 
 export default PokemonsPage;
