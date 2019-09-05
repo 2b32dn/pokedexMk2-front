6b5441eb923de995927d0cc46d9e69d18a275153
2b32dn <DicksonT_Lee@hotmail.com>
2019-09-05 00:39:40 -0400

    convert indentation to space

--- src/containers/PokemonsList.js
+++ src/containers/PokemonsList.js
@@ -4,36 +4,36 @@ import Capitalize from '../tools/Capitalize';
 // import Modal from '../components/modal/Modal'
 
 const PokemonsList = ({ pokemons, show, targetPokemon, pokemonExtra }) => {
-	// async function loadingPokemonData(id) {
-	// 	try {
-	// 		const pokemon = await Promise.all(targetPokemon(id), pokemonExtra(id));
-	// 		return pokemon;
-	// 	} catch (err) {
-	// 		console.log(err);
-	// 	}
-	// }
+  // async function loadingPokemonData(id) {
+  // 	try {
+  // 		const pokemon = await Promise.all(targetPokemon(id), pokemonExtra(id));
+  // 		return pokemon;
+  // 	} catch (err) {
+  // 		console.log(err);
+  // 	}
+  // }
 
-	return (
-		<div className="pokemon-main">
-			{pokemons.map((base_info) => (
-				<div key={base_info.name} className="pokemon-spotlight">
-					<img alt={base_info.name} src={base_info.sprites.front_default} className="pokemon-image" />
-					<button
-						onClick={() => {
-							// loadingPokemonData(base_info.id);
-							show();
-							targetPokemon(base_info.id);
-							pokemonExtra(base_info.id);
-						}}
-						value={base_info.id}
-						className="pokmon-button"
-					>
-						{Capitalize(base_info.species.name)}
-					</button>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="pokemon-main">
+      {pokemons.map((base_info) => (
+        <div key={base_info.name} className="pokemon-spotlight">
+          <img alt={base_info.name} src={base_info.sprites.front_default} className="pokemon-image" />
+          <button
+            onClick={() => {
+              // loadingPokemonData(base_info.id);
+              show();
+              targetPokemon(base_info.id);
+              pokemonExtra(base_info.id);
+            }}
+            value={base_info.id}
+            className="pokmon-button"
+          >
+            {Capitalize(base_info.species.name)}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PokemonsList;
